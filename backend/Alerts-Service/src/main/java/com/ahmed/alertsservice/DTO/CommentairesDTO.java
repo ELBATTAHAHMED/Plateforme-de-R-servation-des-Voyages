44d1a79a22a7d2053492dc33c21f67329c4a3cf8
ELBATTAHAHMED <ahmedelbatth123@gmail.com>
2025-02-07 12:10:15 +0100

    uploading project 1

--- backend/Alerts-Service/src/main/java/com/ahmed/alertsservice/DTO/CommentairesDTO.java
+++ backend/Alerts-Service/src/main/java/com/ahmed/alertsservice/DTO/CommentairesDTO.java
@@ -1,55 +1,15 @@
 package com.ahmed.alertsservice.DTO;
 
 import java.time.LocalDateTime;
+import lombok.*;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentairesDTO {
-
-
-
 	private Long id_comment;
-
 	private String contenu;
-
 	private LocalDateTime dateHeureComment;
-	
-	
-
-	public CommentairesDTO() {}
-
-
-	public CommentairesDTO(Long id_comment, String contenu, LocalDateTime dateHeureComment) {
-		super();
-		this.id_comment = id_comment;
-		this.contenu = contenu;
-		this.dateHeureComment = dateHeureComment;
-	}
-	public Long getId_comment() {
-		return id_comment;
-	}
-	public void setId_comment(Long id_comment) {
-		this.id_comment = id_comment;
-	}
-	public String getContenuComment() {
-		return contenu;
-	}
-	public void setContenuComment(String contenuComment) {
-		this.contenu = contenuComment;
-	}
-	public LocalDateTime getDateHeureComment() {
-		return dateHeureComment;
-	}
-	public void setDateHeureComment(LocalDateTime dateHeureComment) {
-		this.dateHeureComment = dateHeureComment;
-	}
-
-	@Override
-	  public String toString() {
-	    return "Activitee [id_Commentaire=" +id_comment + ", contenu_Commentaire=" + contenu
-	        + ", Date_Commenatire=" + dateHeureComment  +  "]" ;
-	  }
-
 	
 }
--- backend/Alerts-Service/src/main/java/com/ahmed/alertsservice/DTO/ReservationDTO.java
+++ backend/Alerts-Service/src/main/java/com/ahmed/alertsservice/DTO/ReservationDTO.java
@@ -1,7 +1,15 @@
 package com.ahmed.alertsservice.DTO;
 
+import lombok.*;
+
 import java.sql.Date;
 
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationDTO {
 
     private Long id_reservation;
@@ -10,79 +18,4 @@ public class ReservationDTO {
     private Double prixTotalReservation;
     private String statutReservation;
     private Long id_utilisateur; // Identifiant de l'utilisateur associé à la réservation.
-
-    // Constructeur par défaut
-    public ReservationDTO() {
-    }
-
-    // Constructeur avec tous les champs
-    public ReservationDTO(Long id_reservation, Date dateReservation, Long dureeReservation, Double prixTotalReservation, String statutReservation, Long id_utilisateur) {
-        this.id_reservation = id_reservation;
-        this.dateReservation = dateReservation;
-        this.dureeReservation = dureeReservation;
-        this.prixTotalReservation = prixTotalReservation;
-        this.statutReservation = statutReservation;
-        this.id_utilisateur = id_utilisateur;
-    }
-
-    // Getters et setters
-    public Long getId_reservation() {
-        return id_reservation;
-    }
-
-    public void setId_reservation(Long id_reservation) {
-        this.id_reservation = id_reservation;
-    }
-
-    public Date getDateReservation() {
-        return dateReservation;
-    }
-
-    public void setDateReservation(Date dateReservation) {
-        this.dateReservation = dateReservation;
-    }
-
-    public Long getDureeReservation() {
-        return dureeReservation;
-    }
-
-    public void setDureeReservation(Long dureeReservation) {
-        this.dureeReservation = dureeReservation;
-    }
-
-    public Double getPrixTotalReservation() {
-        return prixTotalReservation;
-    }
-
-    public void setPrixTotalReservation(Double prixTotalReservation) {
-        this.prixTotalReservation = prixTotalReservation;
-    }
-
-    public String getStatutReservation() {
-        return statutReservation;
-    }
-
-    public void setStatutReservation(String statutReservation) {
-        this.statutReservation = statutReservation;
-    }
-
-    public Long getId_utilisateur() {
-        return id_utilisateur;
-    }
-
-    public void setId_utilisateur(Long id_utilisateur) {
-        this.id_utilisateur = id_utilisateur;
-    }
-
-    @Override
-    public String toString() {
-        return "ReservationDTO{" +
-                "id_reservation=" + id_reservation +
-                ", dateReservation=" + dateReservation +
-                ", dureeReservation=" + dureeReservation +
-                ", prixTotalReservation=" + prixTotalReservation +
-                ", statutReservation='" + statutReservation + '\'' +
-                ", id_utilisateur=" + id_utilisateur +
-                '}';
-    }
 }
